3274a0ead70a4ead41746e32461b938062490111
unknown <malexandresantos@hotmail.com>
2023-04-19 14:15:59 -0300

    Criação do metodo CRUD de uma farmacia.

---
+++ Farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,79 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome do produto é obrigatório!")
+	@Size(max = 50, message = "O nome contem um máximo de 50 caracteres!")
+	private String nome;
+	
+	@NotBlank(message = "O valor é obrigatório!")
+	private String valor;
+	
+	@NotBlank(message = "Digite uma recomendação breve de uso!")
+	private String recomendacao;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public String getRecomendacao() {
+		return recomendacao;
+	}
+
+	public void setRecomendacao(String recomendacao) {
+		this.recomendacao = recomendacao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
+	
+}
